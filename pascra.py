94da132dac9bd407432d1d0775450f0ecb24ac5d
Ailete619 <loicinosaka@gmail.com>
2016-01-26 19:35:10 +0900

    can now do simple 1 cookie login, use name for scraping results, can scrap tabular data and send it back in a tabular format

--- pascra.py
+++ pascra.py
@@ -2,7 +2,7 @@
 from google.appengine.api import taskqueue, users
 import json
 import logging
-from scrap import PageScraper
+from scrap import PageScraper, WebsiteScraper
 import urllib
 import urllib2
 import webapp2
@@ -24,11 +24,83 @@ class MiscHandler(BaseHandler):
     def post(self,**kwargs):
         self.render_response('/pascra.html', **kwargs)
 
-class PostTestingHandler(BaseHandler):
+class RequestTestingHandler(BaseHandler):
     def get(self,**kwargs):
-        self.render_response('/pascra_post.html', **kwargs)
+        test_kukkiapple_order_scrapping =   {
+                                             "login":{"url":"https://apple.kukkia.jp/bin/login.php","login":{"name":"login_id","value":"kukkia"},"password":{"name":"login_pw","value":"kukkiapple"},"fields":[{"name":"bst","value":"1024"},{"name":"bsy","value":"1280"}]},
+                                             "scrap_list":[
+                                                             {
+                                                              "encoding":"utf-8",
+                                                              "fields":[{"name":"cmd","value":"form"},
+                                                                        {"name":"flg","value":"0"},
+                                                                        {"name":"bst","value":"1024"},
+                                                                        {"name":"bsy","value":"1280"}],
+                                                              "for_field":{"name":"pi_id","values":["1"]},
+                                                              "selectors":{
+                                                                           "order_date":{"string":"input#pi_date","extractor":{"type":"attribute","name":"value"}},
+                                                                           "customer_code":{"string":"input#pi_cus_code","extractor":{"type":"attribute","name":"value"}},
+                                                                           "customer_name":{"string":"input#pi_customer_name","extractor":{"type":"attribute","name":"value"}},
+                                                                           "customer_address":{"string":"textarea#pi_sold_to","extractor":{"type":"method","name":"text"}},
+                                                                           "shipping_address":{"string":"textarea#pi_ship_to","extractor":{"type":"method","name":"text"}},
+                                                                           "order_delivery_date":{"string":"input#pi_delivery_date","extractor":{"type":"attribute","name":"value"}},
+                                                                           "order_delivery_way":{"string":"input#pi_way_of_deli","extractor":{"type":"attribute","name":"value"}},
+                                                                           "order_terms":{"string":"input#pi_terms","extractor":{"type":"attribute","name":"value"}},
+                                                                           "exchange_rate":{"string":"input#pi_rate","extractor":{"type":"attribute","name":"value"}},
+                                                                           "subtotal":{"string":"td#sub_total","extractor":{"type":"text"}},
+                                                                           "discount_percentage":{"string":"input#discount_par","extractor":{"type":"attribute","name":"value"}},
+                                                                           "discount":{"string":"input#pi_discount","extractor":{"type":"attribute","name":"value"}},
+                                                                           "shipping_cost":{"string":"input#pi_shipping_cost","extractor":{"type":"attribute","name":"value"}},
+                                                                           "bank_handling_charge":{"string":"input#pi_bank_handling_charge","extractor":{"type":"attribute","name":"value"}},
+                                                                           "total":{"string":"td#grand_total","extractor":{"type":"text"}},
+                                                                           "memo":{"string":"textarea#pi_memo","extractor":{"type":"method","name":"text"}},
+                                                                           },
+                                                              "tabular_selectors":{
+                                                                                   "order_lines":{"line_selector":"div#ajax_area tr",
+                                                                                                  "cell_selectors":[
+                                                                                                                    {"name":"line_number","string":"td:first-child","extractor":{"type":"text"}},
+                                                                                                                    {"name":"reference","string":"td:nth-child(2) input[type=text]","extractor":{"type":"attribute","name":"value"}},
+                                                                                                                    {"name":"name","string":"td:nth-child(2) span","extractor":{"type":"text"}},
+                                                                                                                    {"name":"warehouse","string":"td:nth-child(3) select option[selected]","extractor":{"type":"text"}},
+                                                                                                                    {"name":"price","string":"td:nth-child(4) input","extractor":{"type":"attribute","name":"value"}},
+                                                                                                                    {"name":"quantity","string":"td:nth-child(5) input","extractor":{"type":"attribute","name":"value"}},
+                                                                                                                    {"name":"backorder","string":"td:nth-child(6) input","extractor":{"type":"attribute","name":"value"}},
+                                                                                                                    {"name":"amount","string":"td:nth-child(7) input","extractor":{"type":"attribute","name":"value"}},
+                                                                                                                    ]
+                                                                                                  }
+                                                                                   },
+                                                              "url":"https://apple.kukkia.jp/bin/ovs_pi.php",
+                                                              }
+                                                           ]
+                                            }
+        test_kukkiapple_order_list_scrapping =  {
+                                                 "login":{"url":"https://apple.kukkia.jp/bin/login.php","login":{"name":"login_id","value":"kukkia"},"password":{"name":"login_pw","value":"kukkiapple"},"fields":[{"name":"bst","value":"1024"},{"name":"bsy","value":"1280"}]},
+                                                 "scrap_list":[
+                                                                 {
+                                                                  "encoding":"utf-8",
+                                                                  "fields":[{"name":"cmd","value":"list"},
+                                                                            {"name":"stxt","value":""},
+                                                                            {"name":"s_cus_id","value":""},
+                                                                            {"name":"date_st","value":""},
+                                                                            {"name":"date_en","value":""},
+                                                                            {"name":"bst","value":"1024"},
+                                                                            {"name":"bsy","value":"1280"}
+                                                                            ],
+                                                                  "selectors":{"edit_onclick":{"string":"div#ajax_area tr td:nth-child(8) a[onclick]","extractor":{"type":"attribute","name":"onclick"}},
+                                                                               "order_id":{"string":"div#ajax_area tr td:nth-child(1)","extractor":{"type":"text"}}
+                                                                               },
+                                                                  "urls":["https://apple.kukkia.jp/bin/ovs_pi.php"]
+                                                                  },
+                                                        ]
+                                                }
+        s = WebsiteScraper()
+        scraps = s.scrap(test_kukkiapple_order_list_scrapping)
+        logging.info(scraps)
+        self.render_response('/pascra_request.html', **kwargs)
+
     def post(self,**kwargs):
         testdata = {"url":"http://www6.nhk.or.jp/kokusaihoudou/archive/archive.html?i=151106","encoding":"utf-8","selectors":["nav>ul>li>a"]}
+        testdata = {"login":{"url":"https://apple.kukkia.jp/bin/login.php","login":{"name":"login_id","value":"kukkia"},"password":{"name":"login_pw","value":"kukkiapple"},"fields":[{"name":"bst","value":"1024"},{"name":"bsy","value":"1280"}]},
+                    }
         url = self.request.get("url")
         if not url:
             logging.error("No URL to scrap!")
@@ -43,22 +115,23 @@ class PostTestingHandler(BaseHandler):
         logging.info('response')
         #logging.info(response.read())
         self.response.write(response.read())
-        
 
 class ScrappingHandler(BaseHandler):
     def get(self,**kwargs):
         self.render_response('/pascra.html', **kwargs)
     def post(self,**kwargs):
-        response = PageScraper.fetch(self.request.get('json'))
+        scrapper = WebsiteScraper()
+        scraps = scrapper.scrap(self.request.get('json'))
         logging.info('response')
-        logging.info(response)
-        self.response.write(json.dumps(response))
+        logging.info(scraps)
+        self.response.write(json.dumps(scraps))
         
+
 
 config = {}
 
 app = webapp2.WSGIApplication([
-                               webapp2.Route(r'/scrap/page/post', PostTestingHandler),
+                               webapp2.Route(r'/scrap/page/request', RequestTestingHandler),
                                webapp2.Route(r'/scrap/page', ScrappingHandler),
                                ('.*', MiscHandler)
                               ],
--- scrap.py
+++ scrap.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 '''
 
@@ -13,6 +14,8 @@ from lxml import etree
 from lxml.cssselect import CSSSelector
 from lxml.html import parse, fromstring
 import re
+import urllib
+import urllib2
 import webapp2
 from datetime import date
 
@@ -41,4 +44,160 @@ class PageScraper(object):
                 scraps["data"][selector] = []
                 for dataItem in selectorData(tree):
                     scraps["data"][selector].append(etree.tostring(dataItem, method='html', encoding="utf-8"))
+        return scraps
+class WebsiteScraper(object):
+    websiteCookies = {}
+    def login(self, data):
+        http_headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        post_data = {data["login"]["name"]:data["login"]["value"],data["password"]["name"]:data["password"]["value"]}
+        for field in data["fields"]:
+            post_data[field["name"]] = field["value"]
+        post_data_encoded = urllib.urlencode(post_data)
+        response = urlfetch.fetch(url=data["url"],payload=post_data_encoded,method=urlfetch.POST,headers=http_headers)
+        if response.status_code == 200:
+            cookie = response.headers.get('set-cookie')
+            cookiePairList = [cookiePair.split("=") for cookiePair in cookie.split(";")]
+            self.websiteCookies[cookiePairList[0][0]] = {"value":cookiePairList[0][1]}
+            return True
+        return False
+    def sendRequest(self, url, method=None, data=None, headers=None):
+        cookieString = ""
+        for cookieName, cookieData in self.websiteCookies.iteritems():
+            cookieString += cookieName+"="+cookieData["value"]+";"
+        http_headers = {'Content-Type': 'application/x-www-form-urlencoded','Cookie':cookieString}
+        if headers is not None:
+            http_headers.update(headers)
+        post_data_encoded = urllib.urlencode(data)
+        if method == None:
+            if data == None:
+                method = urlfetch.GET
+            else:
+                method = urlfetch.POST
+        return urlfetch.fetch(url=url,payload=post_data_encoded,method=method,headers=http_headers)
+    def scrapPage(self, response, item):
+        pageScraps = {}
+        pageScraps["status"] = response.status_code
+        if response.status_code == 200:
+            # parse the page
+            parser = etree.HTMLParser()
+            if "encoding" in item:
+                encoding = item["encoding"]
+            else:
+                encoding = "utf-8"
+            tree = etree.parse(StringIO(response.content.decode(encoding)), parser)
+            # extract the data for all the css selectors on the page
+            dataScraps = {}
+            pageScraps["data"] = dataScraps
+            if "selectors" in item:
+                for selectorName, selectorData in item["selectors"].iteritems():
+                    selector = CSSSelector(selectorData["string"])
+                    dataScraps[selectorName] = []
+                    for dataItem in selector(tree):
+                        if "extractor" in selectorData:
+                            extractor = selectorData["extractor"]
+                            if extractor["type"]=="attribute":
+                                if extractor["name"] in dataItem.attrib:
+                                    result = dataItem.attrib[extractor["name"]]
+                                else:
+                                    logging.warning(etree.tostring(dataItem, method='text', encoding="utf-8")+"has no attribute : "+extractor["name"])
+                            elif extractor["type"]=="text":
+                                result = etree.tostring(dataItem, method='text', encoding="utf-8")
+                            elif extractor["type"]=="method":
+                                if extractor["name"]=="text":
+                                    result = dataItem.text
+                                else:
+                                    logging.warning(etree.tostring(dataItem, method='text', encoding="utf-8")+"has no method : "+extractor["name"])
+                            else:
+                                result = etree.tostring(dataItem, method='html', encoding="utf-8")
+                        else:
+                            result = etree.tostring(dataItem, method='html', encoding="utf-8")
+                        if result is not None:
+                            dataScraps[selectorName].append(result)
+            if "tabular_selectors" in item:
+                for tabularSelectorName, tabularSelectorData in item["tabular_selectors"].iteritems():
+                    rowSelector = CSSSelector(tabularSelectorData["line_selector"])
+                    dataScraps[tabularSelectorName] = []
+                    for rowData in rowSelector(tree):
+                        rowScraps = []
+                        dataScraps[tabularSelectorName].append(rowScraps)
+                        for cellSelectorData in tabularSelectorData["cell_selectors"]:
+                            cellSelector = CSSSelector(cellSelectorData["string"])
+                            cellData = cellSelector(rowData)
+                            result = ""
+                            if len(cellData)==1:
+                                dataItem = cellSelector(rowData)[0]
+                                if "extractor" in cellSelectorData:
+                                    extractor = cellSelectorData["extractor"]
+                                    if extractor["type"]=="attribute":
+                                        if extractor["name"] in dataItem.attrib:
+                                            result = dataItem.attrib[extractor["name"]]
+                                        else:
+                                            logging.warning(etree.tostring(dataItem, method='text', encoding="utf-8")+"has no attribute : "+extractor["name"])
+                                    elif extractor["type"]=="text":
+                                        result = etree.tostring(dataItem, method='text', encoding="utf-8")
+                                    elif extractor["type"]=="method":
+                                        if extractor["name"]=="text":
+                                            result = dataItem.text
+                                        else:
+                                            logging.warning(etree.tostring(dataItem, method='text', encoding="utf-8")+"has no method : "+extractor["name"])
+                                    else:
+                                        result = etree.tostring(dataItem, method='html', encoding="utf-8")
+                                else:
+                                    result = etree.tostring(dataItem, method='html', encoding="utf-8")
+                            else:
+                                logging.info(cellSelectorData["string"]+"->"+str(cellData))
+                            rowScraps.append(result)
+        return pageScraps
+    def scrapURL(self, url, item):
+        post_data = {}
+        for field in item["fields"]:
+            post_data[field["name"]] = field["value"]
+        response = self.sendRequest(url=url,data=post_data)
+        return self.scrapPage(response, item)
+    def scrapURLForField(self, item):
+        itemScraps = {}
+        if "urls" in item:
+            url = item["urls"][0]
+            logging.warning("Error: more than one url in inscrapURLForField()")
+        elif "url" in item:
+            url = [item["url"]]
+        else:
+            logging.warning("Error: no url inscrapURLForField()")
+        fieldName = item["for_field"]["name"]
+        fieldValueList = item["for_field"]["values"]
+        post_data = {}
+        for field in item["fields"]:
+            post_data[field["name"]] = field["value"]
+        for fieldValue in fieldValueList:
+            post_data[fieldName] = fieldValue
+            response = self.sendRequest(url=url,data=post_data)
+            itemScraps[fieldValue] = self.scrapPage(response, item)
+        return itemScraps
+    def scrapURLList(self, item):
+        itemScraps = {}
+        if "urls" in item:
+            urlList = item["urls"]
+        elif "url" in item:
+            urlList = [item["url"]]
+        else:
+            logging.warning("Error: no url inscrapURLList()")
+        for url in urlList:
+            if url in itemScraps:
+                urlScraps = itemScraps[url]
+            else:
+                urlScraps = {}
+                itemScraps[url] = urlScraps
+            urlScraps.update(self.scrapURL(url,item))
+        return itemScraps
+    def scrap(self, data):
+        # tabular data output
+        scraps = []
+        if "login" in data:
+            self.login(data["login"])
+        scrapList = data["scrap_list"]
+        for item in scrapList:
+            if "for_field" in item:
+                scraps.append(self.scrapURLForField(item))
+            else:
+                scraps.append(self.scrapURLList(item))
         return scraps
